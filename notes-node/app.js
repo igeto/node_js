305c6e60c192d50f664641306d594451cbbe8f98
Igor Igeto Mitkovski <igetom@gmail.com>
2018-01-16 17:29:49 +0100

    Added funcs for crud using yargs and passing arguments through console

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,6 +2,31 @@ console.log('Starting app.js')
 
 const fs = require('fs')
 const _ = require('lodash')
+const yargs = require('yargs')
 
 const notes = require('./notes.js')
 
+const argv = yargs.argv
+let command = process.argv[2]
+console.log(`Process: ${process.argv}
+Yargs: `, argv)
+
+console.log(`Command: ${command}`)
+
+switch (command) {
+    case 'add':
+        notes.addNote(argv.title, argv.body)
+        break;
+    case 'list':
+        notes.getAll()
+        break
+    case 'read':
+        notes.getNote(argv.title)
+        break
+    case 'remove':
+        notes.removeNote(argv.title)
+        break
+    default:
+        console.log('Command not recognized')
+        break;
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,5 +1,54 @@
 console.log('Starting notes.js')
 
-module.exports.add = (a, b) => {
-    return a + b;
+let notes = [
+    {
+        title: 'first',
+        body: 'My first note'
+    },
+    {
+        title: 'second',
+        body: 'My second note'
+    },
+    {
+        title: 'third', 
+        body: 'My third note'
+    },
+    {
+        title: 'fourth', 
+        body: 'My fourth note'
+    }
+]
+
+function Note(title, body) {
+    this.title = title;
+    this.body = body
+}
+
+let addNote = (title, body) => {
+    console.log(`Adding note ${title} ${body}`)
+    notes.push(new Note(title, body))
+    console.log(notes)
+}
+
+let getAll = () => {
+    console.log(`Getting all notes`)
+    notes.forEach(n => console.log(`${n.title}: ${n.body}`))
+}
+
+let getNote = (title) => {
+    let note = notes.filter(n => n.title === title)
+    console.log(`${note[0].title}: ${note[0].body}`)
+}
+
+let removeNote = (title) => {
+    console.log(`Removing note with title ${title}`)
+    notes = notes.filter(n => n.title !== title)
+    getAll()
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 }
